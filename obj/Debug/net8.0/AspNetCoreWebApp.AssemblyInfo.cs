f4d8ee93d3df8961adb88e3d50c1989636f404f9
MisaVPio <misavpio@outlook.com>
2024-08-28 11:25:42 -0300

    error continues

--- obj/Debug/net8.0/AspNetCoreWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/AspNetCoreWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ad6d98621b55876ed35f9f5a5a40aa251fc36c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e5bba975b2e347797b85e2855898ea6f19e838")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
